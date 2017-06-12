25f388ad1c4edec7a26e73995dcb4276b38fd784
piyapat_d <diablokung1@gmail.com>
2017-06-12 08:33:04 +0700

    add php

--- scripts/js/app.js
+++ scripts/js/app.js
@@ -1,5 +1,3 @@
-
-
 function isNumber(evt) {
     evt = (evt) ? evt : window.event;
     var charCode = (evt.which) ? evt.which : evt.keyCode;
@@ -13,6 +11,32 @@ $('.index-totop').click(function(){
     $('html, body').animate({scrollTop:$('#regis-headline').offset().top}, 'slow');
 });
 
-$('#index-btn-regis-submit').click(function(){
-    window.location.href = 'thanks.html';
-})
+$("#index-btn-regis-submit").on("click", function(e) {
+    if ($("input[name=register_name]").val() === '') {
+        alert('กรุณาใส่ชื่อเพื่อการติดต่อกลับด้วยค่ะ');
+        return;
+    }
+    if ($("input[name=register_tel]").val() === '') {
+        alert('กรุณาใส่หมายเลขโทรศัพท์เพื่อการติดต่อกลับด้วยค่ะ');
+        return;
+    }
+    if (!validateEmail($("input[name=register_email]").val())) {
+        alert('กรุณาใส่อีเมลเพื่อการติดต่อกลับด้วยค่ะ');
+        return;
+    }
+
+    $.post("scripts/php/register.php", $("#register-form").serialize(), function(response) {
+        $("#register-form")[0].reset();
+        if (response === 'done') {
+            // alert("ขอบคุณที่ทำการลงทะเบียนค่ะ");
+            window.location.href = 'thanks.html';
+        } else {
+            alert("ไม่สามารถลงทะเบียนได้ในขณะนี้ กรุณาลงทะเบียนใหม่ในภายหลัง");
+        }
+    });
+});
+
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
--- scripts/php/register.php
+++ scripts/php/register.php
@@ -1,28 +1,52 @@
 <?php
-$dbserver = "localhost";
-$databasename = "ljjbroa123_ljjbr";
-$dbuser = "ljjbroa123_ljjbr";
-$dbpass = "fbuxg";
 
-$connect = mysqli($dbserver, $dbuser, $dbpass, $dbname); // เชือมต่อฐานข้อมูล
-if ($connect->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+try{
+    $servername = "localhost";
+    $username = "ljjbroa123_ljjbr";
+    $password = "fbuxg";
+    $db_name = "ljjbroa123_ljjbr";
 
-mysqli_query("set NAMES utf8"); // กำหนดชุดอักขระ
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $db_name);
 
-$sql = "SELECT name, tel, email FROM MyGuests";
-$result = $conn->query($sql);
-
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["name"]. " - Name: " . $row["tel"]. " " . $row["email"]. "<br>";
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
     }
-} else {
-    echo "0 results";
-}
-$conn->close();
 
-?>
+    // This method needs PHP 5 >= 5.5.0 and MySQL 5.6+
+    // mysqli_begin_transaction($conn, MYSQLI_TRANS_START_READ_WRITE);
+
+    $name = $_POST['register_name'];
+    $tel = $_POST['register_tel'];
+    $email = $_POST['register_email'];
+    $event = $_POST['register_event'];
+    $budget = 1;
+    $current_datetime = date("Y-m-d H:i:s");
+    $date = date_create($current_datetime);
+    $dd = date_format($date, "d");
+    $mm = date_format($date, "m");
+    $code = 'XS' . $dd . $mm;
+
+    $budgets = array("เงินสดรวมขอสินเชื่อ 10 ล้านลงมา", "สามารถขอสินเชื่อ 10 ล้านขึ้นไป", "ซื้อด้วยเงินสด 10-15 ล้าน", "ซื้อด้วยเงินสด หรือ สินเชื่อ เกิน 15 ล้านขึ้นไป");
+    $infos = array("ให้โทรติดต่อกลับด่วน", "ติดต่อกลับภายในอาทิตย์นี้", "สนใจส่งรายละเอียดทาง email");
+
+    // Make data to be able to display in Thai.
+    $sql = "SET NAMES 'utf8'";
+    $conn->query($sql) or die('Error query: ' . mysqli_error());
+
+    $sql = "INSERT INTO register (name, tel, email, budget, need_info, created_date)
+			VALUES ('$name', '$tel', '$email', $budget, $event, '$current_datetime')";
+
+    if (!mysqli_query($conn, $sql)) {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
+
+    echo "done";
+    he
+    die();
+}catch(Exception $e){
+    mysqli_rollback($conn);
+}
